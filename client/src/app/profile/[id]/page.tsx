8105488bcf2171cd4e2df7a250bc849401098c93
Astrionix <padalalmrreddy@gmail.com>
2025-12-02 15:44:27 +0530

    fix: restore 'use client' directive

--- client/src/app/profile/[id]/page.tsx
+++ client/src/app/profile/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { API_URL } from "@/config";
 
 import { useEffect, useState } from "react";
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { API_URL } from "@/config";
 
 import { useState } from "react";
